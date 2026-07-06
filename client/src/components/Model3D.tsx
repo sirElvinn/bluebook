c2e2b9831695f83764c163a312c2909d2145bef3
Aditya Thakur <adityathakur6176@gmail.com>
2026-07-06 21:10:08 +0545

    .obj -> .glb

--- client/src/components/Model3D.tsx
+++ client/src/components/Model3D.tsx
@@ -1,26 +1,6 @@
-import { useLoader } from '@react-three/fiber';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-import { TextureLoader, MeshStandardMaterial } from 'three';
-import { useMemo } from 'react';
+import { useGLTF } from '@react-three/drei';
 
 export default function Model3D() {
-  const obj = useLoader(OBJLoader, '/models/base.obj');
-
-  const diffuse = useLoader(TextureLoader, '/models/texture_diffuse.png');
-  const normal = useLoader(TextureLoader, '/models/texture_normal.png');
-  const roughness = useLoader(TextureLoader, '/models/texture_roughness.png');
-  const metallic = useLoader(TextureLoader, '/models/texture_metallic.png');
-
-  const material = useMemo(() => new MeshStandardMaterial({
-    map: diffuse,
-    normalMap: normal,
-    roughnessMap: roughness,
-    metalnessMap: metallic,
-  }), [diffuse, normal, roughness, metallic]);
-
-  obj.traverse((child: any) => {
-    if (child.isMesh) child.material = material;
-  });
-
-  return <primitive object={obj} scale={1.4} position={[0, -1.2, 0]} rotation={[0, -0.5, 0]} />;
+  const { scene } = useGLTF('/models/base_basic_pbr.glb');
+  return <primitive object={scene} scale={1.4} position={[0, -1.4, 0]} rotation={[0, -0.5, 0]} />;
 }
