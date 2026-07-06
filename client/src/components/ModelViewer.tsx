efb015c2a87cd7538b5552c3441ed07bc2b5536a
Aditya Thakur <adityathakur6176@gmail.com>
2026-07-06 13:11:38 +0545

    Zoom disabled on 3d and container fit for 3d

--- client/src/components/ModelViewer.tsx
+++ client/src/components/ModelViewer.tsx
@@ -8,10 +8,10 @@ export default function ModelViewer() {
     <div style={{ width: '100%', height: '500px' }}>
       <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
         <Suspense fallback={null}>
-          <Stage environment="city" intensity={0.5}>
+          <Stage environment="city" intensity={0.5} adjustCamera={1.6}>
             <Model3D />
           </Stage>
-          <OrbitControls />
+          <OrbitControls enableZoom={false} />
         </Suspense>
       </Canvas>
     </div>
